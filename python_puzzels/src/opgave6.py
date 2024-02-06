2d9bedf7163984385ef98a434efed0334cfb072d
Tobias0107 <149398439+Tobias0107@users.noreply.github.com>
2024-02-06 21:04:00 +0100

    Eerste poging opgave 6

--- python_puzzels/src/opgave6.py
+++ python_puzzels/src/opgave6.py
@@ -1,26 +1,31 @@
 class Event:
     """ Represents an event that happens at a certain time."""
-    
+
     def __init__(self, time, description):
         """ Initialises an Event object with a 'time' object of type Time and a
-        'description' of type str. 
+        'description' of type str.
         >>> event = Event(Time(18, 30, 0), "dinner")
         """
+        self.time = time
+        self.description = description
 
     def __repr__(self):
-        """ Returns the string representation of an Event object. 
+        """ Returns the string representation of an Event object.
         >>> print( Event(Time(18, 30, 0), "dinner") )
         18:30:00 dinner
         """
+        return self.time + " " + self.description
 
     def get_time(self):
-        """ Returns the time of an Event object. 
+        """ Returns the time of an Event object.
         >>> print( Event(Time(18, 30, 0), "dinner").get_time() )
         18:30:00
         """
+        return self.time
 
     def get_description(self):
-        """ Returns the description of an Event object. 
+        """ Returns the description of an Event object.
         >>> print( Event(Time(18, 30, 0), "dinner").get_description() )
         dinner
         """
+        return self.description
