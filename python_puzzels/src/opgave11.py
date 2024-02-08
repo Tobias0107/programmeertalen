e6d8bad6d061b707cec142246f5353b7a83bd583
tobias0107 <tobias.van.den.bosch@student.uva.nl>
2024-02-08 14:21:26 +0100

    Opgave 2 klaar

--- python_puzzels/src/opgave11.py
+++ python_puzzels/src/opgave11.py
@@ -1,2 +1,320 @@
+import time
+from gtts import gTTS
+import playsound
+
+HOURS_IN_DAY = 24
+MINUTES_IN_HOUR = 60
+SECONDS_IN_MINUTE = 60
+
+
+class Time:
+    """ Represents a time of day."""
+    def __init__(self, hours, minutes, seconds):
+        """ Initialises a Time object with integers 'hours', 'minutes' and
+        'seconds.
+        >>> t = Time(18, 30, 0)
+        """
+        while seconds < 0:
+            seconds += SECONDS_IN_MINUTE
+            minutes -= 1
+        while minutes < 0:
+            minutes += MINUTES_IN_HOUR
+            hours -= 1
+        while hours < 0:
+            hours += HOURS_IN_DAY
+        while seconds >= SECONDS_IN_MINUTE:
+            seconds -= SECONDS_IN_MINUTE
+            minutes += 1
+        while minutes >= MINUTES_IN_HOUR:
+            minutes -= MINUTES_IN_HOUR
+            hours += 1
+
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds % SECONDS_IN_MINUTE
+
+    def __repr__(self):
+        """ Returns the string representation of a Time object.
+        >>> print( Time(8,5,30) )
+        08:05:30
+        """
+        hour = str(self.hours)
+        if self.hours < 10:
+            hour = "0" + str(self.hours)
+        minutes = str(self.minutes)
+        if self.minutes < 10:
+            minutes = "0" + str(self.minutes)
+        seconds = str(self.seconds)
+        if self.seconds < 10:
+            seconds = "0" + str(self.seconds)
+        return f"{hour}:{minutes}:{seconds}"
+
+    def get_hours(self):
+        """ Returns the hours of the Time object.
+        >>> Time(23,0,0).get_hours()
+        23
+        """
+        return self.hours
+
+    def get_minutes(self):
+        """ Returns the minutes of the Time object.
+        >>> Time(0,59,0).get_minutes()
+        59
+        """
+        return self.minutes
+
+    def get_seconds(self):
+        """ Returns the seconds of the Time object.
+        >>> Time(0,0,59).get_seconds()
+        59
+        """
+        return self.seconds
+
+    def set_time(self, hours, minutes, seconds):
+        """ Sets the time of the Time object to 'hours', 'minutes',
+        and 'seconds' making sure the values are in valid range:
+          hours:   [0, HOURS_IN_DAY)
+          minutes: [0, MINUTES_IN_HOUR)
+          seconds: [0, SECONDS_IN_MINUTE)
+        >>> time = Time(0, 0, 0)
+        >>> time.set_time(0, 0, 90)
+        >>> print(time)
+        00:01:30
+        >>> time.set_time(0, 0, 3600)
+        >>> print(time)
+        01:00:00
+        >>> time.set_time(0, 0, -1)
+        >>> print(time)
+        23:59:59
+        >>> time.set_time(10, -121, 0)
+        >>> print(time)
+        07:59:00
+        >>> time.set_time(-50, 0, 0)
+        >>> print(time)
+        22:00:00
+        >>> print(Time(10, -120, -150)) # __init__() test
+        07:57:30
+        """
+        while seconds < 0:
+            seconds += SECONDS_IN_MINUTE
+            minutes -= 1
+        while minutes < 0:
+            minutes += MINUTES_IN_HOUR
+            hours -= 1
+        while hours < 0:
+            hours += HOURS_IN_DAY
+        while seconds >= SECONDS_IN_MINUTE:
+            seconds -= SECONDS_IN_MINUTE
+            minutes += 1
+        while minutes >= MINUTES_IN_HOUR:
+            minutes -= MINUTES_IN_HOUR
+            hours += 1
+
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds % SECONDS_IN_MINUTE
+
+    def get_total_seconds(self):
+        """ Returns the number of seconds since time 00:00:00.
+        >>> Time(0,0,1).get_total_seconds()
+        1
+        >>> Time(0,1,0).get_total_seconds()
+        60
+        >>> Time(1,0,0).get_total_seconds()
+        3600
+        >>> Time(13,30,5).get_total_seconds()
+        48605
+        """
+        return (self.hours * 3600 + self.minutes * 60 + self.seconds)
+
+    def __add__(self, other):
+        """ Returns a valid Time objects which is Time objects
+        'other' added to 'self'.
+        >>> print(Time(0,0,0) + Time(1,2,3))
+        01:02:03
+        >>> print(Time(13,30,0) + Time(1,46,-45))
+        15:15:15
+        """
+        hour = self.hours + other.hours
+        minute = self.minutes + other.minutes
+        second = self.seconds + other.seconds
+        # while second > SECONDS_IN_MINUTE:
+        #     second -= SECONDS_IN_MINUTE
+        #     minute += 1
+        # while minute > MINUTES_IN_HOUR:
+        #     minute -= MINUTES_IN_HOUR
+        #     hour += 1
+        return Time((hour % HOURS_IN_DAY), minute, second)
+
+    def __sub__(self, other):
+        """ Returns a valid Time objects which is Time objects
+        'other' substracted from 'self'.
+        >>> print(Time(10,10,10) - Time(1,2,3))
+        09:08:07
+        >>> print(Time(10,0,0) - Time(1,50,600))
+        08:00:00
+        """
+        hour = self.hours - other.hours
+        minute = self.minutes - other.minutes
+        second = self.seconds - other.seconds
+        return Time((hour % HOURS_IN_DAY), minute, second)
+
+
+def get_current_hours_minutes_seconds():
+    """ Returns the current (hours, minutes, seconds) as a tuple. """
+    t = time.localtime()
+    return (t.tm_hour, t.tm_min, t.tm_sec)
+
+
+def now():
+    """ Returns the current time as Time object. """
+    return Time(*get_current_hours_minutes_seconds())
+
+
+class Event:
+    """ Represents an event that happens at a certain time."""
+
+    def __init__(self, time, description):
+        """ Initialises an Event object with a 'time' object of type Time and a
+        'description' of type str.
+        >>> event = Event(Time(18, 30, 0), "dinner")
+        """
+        self.time = time
+        self.description = description
+
+    def __repr__(self):
+        """ Returns the string representation of an Event object.
+        >>> print( Event(Time(18, 30, 0), "dinner") )
+        18:30:00 dinner
+        """
+        return str(self.time) + " " + self.description
+
+    def get_time(self):
+        """ Returns the time of an Event object.
+        >>> print( Event(Time(18, 30, 0), "dinner").get_time() )
+        18:30:00
+        """
+        return self.time
+
+    def get_description(self):
+        """ Returns the description of an Event object.
+        >>> print( Event(Time(18, 30, 0), "dinner").get_description() )
+        dinner
+        """
+        return self.description
+
+
+class AlarmClock:
+    """ Represents an alarm clock that can handle events. """
+
+    def __init__(self):
+        """ Initialises an AlarmClock object with an empty list of events.
+        >>> alarm_clock = AlarmClock()
+        """
+        self.eventlist = []
+
+    def add_event(self, event):
+        """ Adds an 'event' to this AlarmClock object, it doesn't return
+        anything.
+        >>> alarm_clock = AlarmClock()
+        >>> event = Event(Time(18, 30, 0), "dinner")
+        >>> alarm_clock.add_event(event)
+        """
+        self.eventlist.append(event)
+        self.sort()
+
+    def __repr__(self):
+        """ Returns a string representation of the AlarmClock object.
+        >>> alarm_clock = AlarmClock()
+        >>> event = Event(Time(18, 30, 0), "dinner")
+        >>> alarm_clock.add_event(event)
+        >>> s = str(alarm_clock)
+        >>> "18:30:00" in s
+        True
+        >>> "dinner" in s
+        True
+        >>> "breakfast" in s
+        False
+        """
+        string = ""
+        for i in self.eventlist:
+            string = string + str(i)
+        return string
+
+    def __len__(self):
+        """ Returns the number of events in this AlarmClock object.
+        >>> alarm_clock = AlarmClock()
+        >>> len(alarm_clock)
+        0
+        >>> event = Event(Time(18, 30, 0), "dinner")
+        >>> alarm_clock.add_event(event)
+        >>> len(alarm_clock)
+        1
+        """
+        return len(self.eventlist)
+
+    def sort(self):
+        """ Sorts the events by time.
+        >>> alarm_clock = AlarmClock()
+        >>> alarm_clock.add_event( Event(Time(0, 0, 2), "event2") )
+        >>> alarm_clock.add_event( Event(Time(0, 0, 1), "event1") )
+        >>> s = str(alarm_clock)
+        >>> s.find("event1") < s.find("event2")
+        True
+        """
+        self.eventlist.sort(key=lambda x: x.get_time().get_total_seconds())
+
+    def get_next_event(self):
+        """ Returns the next event with the smallest time.
+        >>> alarm_clock = AlarmClock()
+        >>> alarm_clock.add_event( Event(Time(0, 0, 2), "event2") )
+        >>> alarm_clock.get_next_event().get_description()
+        'event2'
+        >>> alarm_clock.add_event( Event(Time(0, 0, 1), "event1") )
+        >>> alarm_clock.get_next_event().get_description()
+        'event1'
+        """
+        return self.eventlist[0]
+
+    def remove_next_event(self):
+        """ Removes and returns the next event with the smallest time.
+        >>> alarm_clock = AlarmClock()
+        >>> alarm_clock.add_event( Event(Time(0, 0, 2), "event2") )
+        >>> alarm_clock.add_event( Event(Time(0, 0, 1), "event1") )
+        >>> alarm_clock.remove_next_event().get_description()
+        'event1'
+        >>> alarm_clock.remove_next_event().get_description()
+        'event2'
+        """
+        return self.eventlist.pop(0)
+
     def wait_for_and_handle_events(self):
         """ Wait for each event to pass and then print the event. """
+        while len(self.eventlist) > 0:
+            event = self.eventlist[0]
+            seconds = (event.get_time() - now()).get_total_seconds()
+            time.sleep(seconds)
+            text_to_speech(event.get_description())
+            self.eventlist.pop(0)
+
+
+def text_to_speech(text):
+    tts = gTTS(text=text, lang='en')
+    filename = "speech.mp3"
+    tts.save(filename)
+    playsound.playsound(filename, True)
+
+
+def main():
+    alarm_clock = AlarmClock()
+    alarm_clock.add_event(Event(Time(0, 0, 10) + now(), "event2"))
+    alarm_clock.add_event(Event(Time(0, 0, 5) + now(), "event1"))
+    alarm_clock.add_event(Event(Time(0, 0, 15) + now(), "event3"))
+
+    # alarm_clock.add_event(Event(Time(1, 1, 1), "event1"))
+    # alarm_clock.add_event(Event(Time(3, 3, 3), "event3"))
+    alarm_clock.wait_for_and_handle_events()
+
+
+if __name__ == "__main__":
+    main()
