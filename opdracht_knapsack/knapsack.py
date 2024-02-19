765b17c629740489696c847543f2035fbf3b8509
tobias0107 <tobias.van.den.bosch@student.uva.nl>
2024-02-19 12:37:57 +0100

    Recursive solver works now

--- opdracht_knapsack/knapsack.py
+++ opdracht_knapsack/knapsack.py
@@ -1,4 +1,5 @@
 import csv
+import copy
 
 MAX_WEIGHT = 110
 MAX_VOLUME = 150
@@ -237,28 +238,27 @@ class Solver_Optimal_Recursive:
                 raise TypeError("Item class expected")
         except Exception as e:
             return items_try
-        print(f"weight item = {item.get_weight()}, volume item = {item.get_volume()}")
-        print(f"weight list = {items_try.get_weight()}, volume list = {items_try.get_volume()}")
         if (item.get_weight() + items_try.get_weight() > max_weight or item.get_volume() + items_try.get_volume() > max_volume):
-            print("\n\n condition activated\n\n")
-            return items_try
-        items_try_no_add = self.recursive_solve(All_items=All_items,
-                                                items_try=items_try,
+            return self.recursive_solve(All_items=All_items,
+                                        items_try=items_try,
+                                        max_weight=max_weight,
+                                        max_volume=max_volume)
+        items_try_no_add = copy.deepcopy(items_try)
+        All_items_copy = copy.deepcopy(All_items)
+        items_try_no_add = self.recursive_solve(All_items=All_items_copy,
+                                                items_try=items_try_no_add,
                                                 max_weight=max_weight,
-                                                max_volume=max_volume)
+                                                max_volume=max_volume
+                                                )
         items_try.add_item(item=item)
-        items_try_add = self.recursive_solve(All_items=All_items,
-                                                items_try=items_try,
-                                                max_weight=max_weight,
-                                                max_volume=max_volume)
-        points_added = items_try_add.get_points()
-        points_not_added = items_try_no_add.get_points()
-        if (points_added < points_not_added):
-            print(f"\n\nreturns item not added = {items_try_no_add}")
-            return items_try_no_add
+        items_try = self.recursive_solve(All_items=All_items,
+                                         items_try=items_try,
+                                         max_weight=max_weight,
+                                         max_volume=max_volume)
+        if (items_try.get_points() > items_try_no_add.get_points()):
+            return items_try
         else:
-            print(f"\n\nreturns item added = {items_try_add}")
-            return items_try_add
+            return items_try_no_add
 
     def get_best_knapsack(self):
         return self.knapsack
