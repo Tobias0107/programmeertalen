76ea213f75aac35ce19a376b7e26513d9dab35c1
tobias0107 <tobias.van.den.bosch@student.uva.nl>
2024-02-28 10:56:17 +0100

    just some tests made for learning haskell

---
+++ test_puzzles_haskell/test.hs
@@ -0,0 +1,11 @@
+import Data.Text.Lazy.Read (double)
+doubleMe :: Num a => a -> a
+doubleMe x = x + x
+doubleUS :: Num a => a -> a -> a
+doubleUS x y = doubleMe x + doubleMe y
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x = if x > 100
+                            then x
+                            else x*2
+doubleSmallNumber' :: (Ord a, Num a) => a -> a
+doubleSmallNumber' x = doubleSmallNumber x + 1
