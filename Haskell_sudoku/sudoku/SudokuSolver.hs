974790798d78afd08602958cc2d5fd15716c9cf1
tobias0107 <tobias.van.den.bosch@student.uva.nl>
2024-03-01 10:42:49 +0100

    .

--- Haskell_sudoku/sudoku/SudokuSolver.hs
+++ Haskell_sudoku/sudoku/SudokuSolver.hs
@@ -1,6 +1,7 @@
 import System.Environment
 import Data.List
 import Distribution.Simple.Program.HcPkg (list)
+import qualified Control.Applicative as yet
 
 type Row = Int
 type Column = Int
@@ -57,15 +58,30 @@ main :: IO ()
 main =
     do args <- getArgs
        sud <- (readSudoku . getSudokuName) args
-       print$freeInRow sud 9
+       print$freeInSubgrid sud (7,7)
        printSudoku sud
 
-freeInRow2 :: Sudoku -> Row -> [Value]
-freeInRow2 sudoku row = [1 .. 9] \\ filter (\s -> sudoku(row,s) /= 0) [1 .. 9]
-
-
-
 freeInRow :: Sudoku -> Row -> [Value]
-freeInRow sudoku row = [1 .. 9] \\ [sudoku (row, x) | x <- [1 .. 9], sudoku (row, x) /= 0]
+freeInRow sudoku row = [1 .. 9] \\ [sudoku (row, x) | x <- [1 .. 9],
+                                    sudoku (row, x) /= 0]
 
--- freeInColumn :: Sudoku -> Column -> [Value]:
+freeInColumn :: Sudoku -> Column -> [Value]
+freeInColumn sudoku col = [1 .. 9] \\ [sudoku (x, col) | x <- [1 .. 9],
+                                    sudoku (x, col) /= 0]
+
+{- The subgrid should have all values from 1-9 once in the subgrid.
+   This function returns a list of the values that are not in the subgrid yet.
+   The subgrid tested is the subgrid that contains the box given by the given
+   row and collumn. (The subgrid of the given Sudoku)
+-}
+freeInSubgrid :: Sudoku -> (Row, Column) -> [Value]
+freeInSubgrid sudoku (row, col) = [1 .. 9] \\ [sudoku (x, y) |
+
+   x <- [startrow .. endrow], y <- [startcol .. endcol], sudoku (x, y) /= 0]
+      where startrow = sum (
+               map (\x -> if row `elem` x then head x else 0) blocks)
+            endrow = startrow + 2
+            startcol = sum (
+               map (\x -> if col `elem` x then head x else 0) blocks)
+            endcol = startcol + 2
+
