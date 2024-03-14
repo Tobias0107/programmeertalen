f39f4bbae9b49ed8ea58e06150066564723b0d29
tobias0107 <tobias.van.den.bosch@student.uva.nl>
2024-03-14 21:49:38 +0100

    .

--- erlang/skeleton (1)/3_dots_and_boxes/grid.erl
+++ erlang/skeleton (1)/3_dots_and_boxes/grid.erl
@@ -1,12 +1,21 @@
 -module(grid).
--export([show_hlines/2, show_vlines/2, print/1]).
+-export([show_hlines/2, show_vlines/2, print/1, new/2, get_wall/3, has_wall/2, add_wall/2]).
 
-% TODO: The other functions.
+new(Width, Height) -> {Width, Height, []}.
 
+get_wall(X,Y,north) -> {{X,Y},{X,Y+1}};
+get_wall(X,Y,south) -> {{X,Y-1},{X,Y}};
+get_wall(X,Y,east) -> {{X,Y},{X+1,Y}};
+get_wall(X,Y,west) -> {{X-1,Y},{X,Y}}.
+
+add_wall(Wall,{Width, Height, List}) -> {Width, Height, List ++ [Wall]}.
+
+has_wall(Wall,{_, _, List}) -> lists:any(fun (X) -> X == Wall end, List).
+
+
+show_hlines(Row, {_, _, [Head | List]}) ->
 % TODO
-show_hlines(Row, Grid) -> "". 
-% TODO
-show_vlines(Row, Grid) -> "".
+show_vlines(Row, Grid) -> "|".
 
 
 % Prints this grid in a structured format
