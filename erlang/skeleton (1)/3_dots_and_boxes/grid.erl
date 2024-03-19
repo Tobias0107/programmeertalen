018218d07d007c5fc839aaf931c1204448d12db9
tobias0107 <tobias.van.den.bosch@student.uva.nl>
2024-03-19 10:44:26 +0100

    Print functie lijkt te werken

--- erlang/skeleton (1)/3_dots_and_boxes/grid.erl
+++ erlang/skeleton (1)/3_dots_and_boxes/grid.erl
@@ -3,8 +3,8 @@
 
 new(Width, Height) -> {Width, Height, []}.
 
-get_wall(X,Y,north) -> {{X,Y},{X,Y+1}};
-get_wall(X,Y,south) -> {{X,Y-1},{X,Y}};
+get_wall(X,Y,north) -> {{X,Y-1},{X,Y}};
+get_wall(X,Y,south) -> {{X,Y},{X,Y+1}};
 get_wall(X,Y,east) -> {{X,Y},{X+1,Y}};
 get_wall(X,Y,west) -> {{X-1,Y},{X,Y}}.
 
@@ -12,11 +12,21 @@ add_wall(Wall,{Width, Height, List}) -> {Width, Height, List ++ [Wall]}.
 
 has_wall(Wall,{_, _, List}) -> lists:any(fun (X) -> X == Wall end, List).
 
+string_hlines(List, Row, Col) ->
+    Bool = lists:any(fun (A) -> A == {{Col, Row-1},{Col,Row}} end, List),
+    case Bool of
+        true -> "--+";
+        false -> "  +"
+    end.
+show_hlines(Row, {Width, _, List}) -> "+" ++ lists:map(fun (Col) -> string_hlines(List, Row, Col) end, (lists:seq(0, Width-1))) ++ "\n".
 
-show_hlines(Row, {_, _, [Head | List]}) ->
-% TODO
-show_vlines(Row, Grid) -> "|".
-
+string_vlines(List, Col, X) ->
+    Bool = lists:any(fun (A) -> A == {{X, Col},{X+1,Col}} end, List),
+    case Bool of
+        true -> "|  ";
+        false -> "   "
+    end.
+show_vlines(Col, {_, Length, List}) -> lists:map(fun (X) -> string_vlines(List, Col, X) end, (lists:seq(-1, Length+1))) ++ "\n".
 
 % Prints this grid in a structured format
 % using the show_Xlines functions.
